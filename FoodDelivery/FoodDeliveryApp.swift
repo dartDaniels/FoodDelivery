4a1e54621116aa368bdc3bdc11b80212a426e22b
danilakazmiruk <danilakazmiruk@mail.ru>
2024-07-18 02:24:02 +0600

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Данила Казмирук on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
